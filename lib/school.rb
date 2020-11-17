8266aab37df85c25f935d65268860d72a1848941
Sarah Thiery <sarahvthiery@gmail.com>
2020-11-17 18:55:52 -0500

    created school roster and passed all tests

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+
+class School
+    def initialize(name)
+        @roster = {}
+        @name = name
+    end
+
+    attr_accessor :roster
+    attr_accessor :name
+
+    def add_student(name, grade)
+        if !@roster[grade]
+            @roster[grade] = []
+        end
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        sort_roster = @roster.map do |grade, student_arr|
+            [grade, student_arr.sort]
+        end
+        return sort_roster.to_h
+    end
+end
